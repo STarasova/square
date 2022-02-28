778c90b1f94a75a1d69eaa1753f2c7433365fb58
sergeytar <sergey.al.tarasov@gmail.com>
2022-02-28 14:13:19 +0200

    Reformed Code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,14 +1,12 @@
 package ru.netology.sqr;
 
-import java.io.ObjectInputValidation;
-
 public class SQRService {
-    public int calculateSquares (int border1, int border2) {
+    public int calculateSquares(int border1, int border2) {
         int squaresCount = 0;
         for (int i = 10; i < 100; i++) {
-            int iSquare = i*i;
+            int iSquare = i * i;
             if ((iSquare >= border1) && (iSquare <= border2)) {
-                squaresCount ++;
+                squaresCount++;
             }
         }
         return squaresCount;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,6 @@
 package ru.netology.sqr;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
 
